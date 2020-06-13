49311b248208f4ec721e280f8536562d7d00a6d6
Phong <xphongvn@gmail.com>
2020-06-13 05:32:57 -0400

    Update GMM for faster testing

--- 04_simple_GMM.py
+++ 04_simple_GMM.py
@@ -45,104 +45,153 @@ for idx, target_dir in enumerate(dirs):
     # set path
     machine_type = os.path.split(target_dir)[1]
 
-    # Check if model is exsiting
-
-    # generate dataset
-    print("============== DATASET_GENERATOR ==============")
-    files = com.file_list_generator(target_dir)
-    train_data = com.list_to_vector_array(files,
-                                          msg="generate train_dataset",
-                                          n_mels=param["feature"]["n_mels"],
-                                          frames=param["feature"]["frames"],
-                                          n_fft=param["feature"]["n_fft"],
-                                          hop_length=param["feature"]["hop_length"],
-                                          power=param["feature"]["power"],
-                                          extra_features=param["feature"]["extra"],
-                                          extra_only=param["feature"]["extra"])
-
-    # fit a Gaussian Mixture Model with two components
-    clf = mixture.GaussianMixture(n_components=10, covariance_type='full', tol=1e-6, max_iter=100,
-                                  init_params='kmeans', verbose=2)
-    clf.fit(train_data)
-    ##########################################################################################
-    # Test time
-    machine_id_list = com.get_machine_id_list_for_test(target_dir)
-
-    # results by type
-    csv_lines.append([machine_type])
-    csv_lines.append(["id", "AUC", "pAUC"])
-    performance = []
-
-    for id_str in machine_id_list:
-        # load test file
-        test_files, y_true = com.test_file_list_generator(target_dir, id_str, True)
-
-        # setup anomaly score file path
-        anomaly_score_csv = "{result}/anomaly_score_{machine_type}_{id_str}.csv".format(
-            result=param["result_directory_tf"],
-            machine_type=machine_type,
-            id_str=id_str)
-        anomaly_score_list = []
-
-        print("\n============== BEGIN TEST FOR A MACHINE ID ==============")
-        y_pred = [0. for k in test_files]
-        for file_idx, file_path in enumerate(test_files):
-            data = com.file_to_vector_array(file_path,
-                                            n_mels=param["feature"]["n_mels"],
-                                            frames=param["feature"]["frames"],
-                                            n_fft=param["feature"]["n_fft"],
-                                            hop_length=param["feature"]["hop_length"],
-                                            power=param["feature"]["power"],
-                                            extra_features=param["feature"]["extra"],
-                                            extra_only=param["feature"]["extra"])
-            prob = clf.score_samples(data)
-            avg_prob = -np.mean(prob)
-            y_pred[file_idx] = avg_prob
-            anomaly_score_list.append([os.path.basename(file_path), y_pred[file_idx]])
-
-        # save anomaly score
-        com.save_csv(save_file_path=anomaly_score_csv, save_data=anomaly_score_list)
-        com.logger.info("anomaly score result ->  {}".format(anomaly_score_csv))
-
-
-        # append AUC and pAUC to lists
-        # Calculate auc and p_auc
-        auc = metrics.roc_auc_score(y_true, y_pred)
-        p_auc = metrics.roc_auc_score(y_true, y_pred, max_fpr=param["max_fpr"])
-
-        print("AUC : {}".format(auc))
-        print("P_AUC : {}".format(p_auc))
-
-        csv_lines.append([id_str.split("_", 1)[1], auc, p_auc])
-        performance.append([auc, p_auc])
-        com.logger.info("AUC : {}".format(auc))
-        com.logger.info("pAUC : {}".format(p_auc))
-
-        print("\n============ END OF TEST FOR A MACHINE ID ============")
-
-
-    # calculate averages for AUCs and pAUCs
-    averaged_performance = np.mean(np.array(performance, dtype=float), axis=0)
-    csv_lines.append(["Average"] + list(averaged_performance))
-    csv_lines.append([])
-
-    # output results
-    result_path = "{result}/{file_name}_{id_str}".format(result=param["result_directory"],
-                                            file_name=param["result_file"], id_str=id_str)
-    com.logger.info("AUC and pAUC results -> {}".format(result_path))
-    com.save_csv(save_file_path=result_path, save_data=csv_lines)
-
-    anomaly_score_csv = "{result}/anomaly_score_{machine_type}.csv".format(result=param["result_directory"],
-                                                                           machine_type=machine_type)
-    com.save_csv(save_file_path=anomaly_score_csv, save_data=anomaly_score_list)
-    com.logger.info("anomaly score result ->  {}".format(anomaly_score_csv))
-
     if param["feature"]["extra_only"]:
         name = "extra_only"
     elif param["feature"]["extra"]:
         name = "logmel_extra"
     else:
         name = "logmel"
-    #save model
-    with open(param["model_directory"] + "/GMM_{}_{}.model".format(name,machine_type), 'wb') as fp:
-        pickle.dump(clf, fp, protocol=4)
+
+    model_file = param["model_directory"] + "/GMM_{}_{}.model".format(name,machine_type)
+    # Check if model is exsiting
+    if os.path.exists(model_file):
+        print("Found existing model at: {}".format(model_file))
+        with open(model_file, "rb") as fp:
+            clf = pickle.load(fp)
+        print("Finished loading model")
+
+    else:
+        # generate dataset
+        print("============== DATASET_GENERATOR ==============")
+        files = com.file_list_generator(target_dir)
+        train_data = com.list_to_vector_array(files,
+                                              msg="generate train_dataset",
+                                              n_mels=param["feature"]["n_mels"],
+                                              frames=param["feature"]["frames"],
+                                              n_fft=param["feature"]["n_fft"],
+                                              hop_length=param["feature"]["hop_length"],
+                                              power=param["feature"]["power"],
+                                              extra_features=param["feature"]["extra"],
+                                              extra_only=param["feature"]["extra"])
+
+        # fit a Gaussian Mixture Model with two components
+        clf = mixture.GaussianMixture(n_components=10, covariance_type='full', tol=1e-6, max_iter=100,
+                                      init_params='kmeans', verbose=2)
+        clf.fit(train_data)
+
+    ##########################################################################################
+    # Test time
+    # Load test data
+    test_files, test_files_label = com.test_file_list_generator(target_dir, id_name="", mode=True)
+    test_data = com.list_to_vector_array(test_files,
+                                         msg="generate test_dataset",
+                                         n_mels=param["feature"]["n_mels"],
+                                         frames=param["feature"]["frames"],
+                                         n_fft=param["feature"]["n_fft"],
+                                         hop_length=param["feature"]["hop_length"],
+                                         power=param["feature"]["power"],
+                                         extra_features=param["feature"]["extra"],
+                                         extra_only=param["feature"]["extra_only"])
+
+    num_files = len(test_files)
+    num_features = test_data.shape[1]
+    # Number of rows for each data file
+    num_rows = int(len(test_data) / num_files)
+    test_data = test_data.reshape(num_files, num_rows, num_features)
+
+    # setup anomaly score file path
+    anomaly_score_csv = "{result}/anomaly_score_{machine_type}_{id_str}.csv".format(
+        result=param["result_directory"],
+        machine_type=machine_type,
+        id_str="all_id")
+    anomaly_score_list = []
+    anomaly_score_dict = {}
+
+    #### Start testing
+    y_predict = [0. for k in test_files]
+    # Loop to get the score
+    for idx, test_file in tqdm(enumerate(test_data), total=num_files):
+        prob = clf.score_samples(test_file)
+        avg_prob = -np.mean(prob)
+        y_predict[idx] = avg_prob
+        anomaly_score_list.append([os.path.basename(test_files[idx]), y_predict[idx]])
+        anomaly_score_dict[os.path.basename(test_files[idx])] = y_predict[idx]
+
+    # save anomaly score for all id
+    com.save_csv(save_file_path=anomaly_score_csv, save_data=anomaly_score_list)
+    com.logger.info("anomaly score result ->  {}".format(anomaly_score_csv))
+
+    performance_csv = "{result}/result_{machine_type}_{id_str}.csv".format(
+        result=param["result_directory"],
+        machine_type=machine_type,
+        id_str="all_id")
+
+    # AUC and PAUC for all
+    auc = metrics.roc_auc_score(test_files_label, y_predict)
+    p_auc = metrics.roc_auc_score(test_files_label, y_predict, max_fpr=param["max_fpr"])
+
+    com.logger.info("AUC for all: {}".format(auc))
+    com.logger.info("pAUC for all: {}".format(p_auc))
+
+    performance_all = []
+    performance_all.append(["AUC", "p_AUC"])
+    performance_all.append([auc, p_auc])
+
+    com.save_csv(save_file_path=performance_csv, save_data=performance_all)
+    com.logger.info("Performance result ->  {}".format(performance_csv))
+
+    # results by type
+    csv_lines.append([machine_type])
+    csv_lines.append(["id", "AUC", "pAUC"])
+    performance = []
+
+    #Write CSV for each ID
+
+    machine_id_list = com.get_machine_id_list_for_test(target_dir)
+    start = 0
+    for i, id_str in enumerate(machine_id_list):
+        # load test file
+        files, y_true = com.test_file_list_generator(target_dir, id_str, True)
+        # setup anomaly score file path
+        anomaly_score_csv_id = "{result}/anomaly_score_{machine_type}_{id_str}.csv".format(
+            result=param["result_directory"],
+            machine_type=machine_type,
+            id_str=id_str)
+        # Get anomaly score from anomaly_score_dict
+        num = len(files)
+        anomaly_score_list_id = []
+        y_pred = []
+        for file in files:
+            y_pred.append(anomaly_score_dict[os.path.basename(file)])
+            anomaly_score_list_id.append([os.path.basename(file), y_pred[-1]])
+        # save anomaly score for each id
+        com.save_csv(save_file_path=anomaly_score_csv_id, save_data=anomaly_score_list_id)
+        com.logger.info("anomaly score result ->  {}".format(anomaly_score_csv_id))
+
+        # append AUC and pAUC to lists
+        # Calculate auc and p_auc
+        auc = metrics.roc_auc_score(y_true, y_pred)
+        p_auc = metrics.roc_auc_score(y_true, y_pred, max_fpr=param["max_fpr"])
+
+        csv_lines.append([id_str.split("_", 1)[1], auc, p_auc])
+        performance.append([auc, p_auc])
+        com.logger.info("AUC : {}".format(auc))
+        com.logger.info("pAUC : {}".format(p_auc))
+
+    # calculate averages for AUCs and pAUCs
+    averaged_performance = np.mean(np.array(performance, dtype=float), axis=0)
+    csv_lines.append(["Average"] + list(averaged_performance))
+    csv_lines.append([])
+
+    # save model
+    if not os.path.exists(model_file):
+        print("Saving model to: {}".format(model_file))
+        with open(model_file, 'wb') as fp:
+            pickle.dump(clf, fp, protocol=4)
+        print("Finished saving model")
+
+# Saving the final results
+result_path = "{result}/{file_name}".format(result=param["result_directory"],
+                                            file_name=param["result_file"])
+com.logger.info("AUC and pAUC results -> {}".format(result_path))
+com.save_csv(save_file_path=result_path, save_data=csv_lines)
